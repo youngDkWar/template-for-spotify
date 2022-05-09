15a3d5a7c5c34627caadd9cac7f97183906dd9d5
Шаркунов Никита Ильич <Nikita.Sharkunov@urfu.me>
2022-05-09 20:27:41 +0500

    Simple API functions is ready

---
+++ implicit_grant/public/data/data.js
@@ -0,0 +1,78 @@
+/**
+ * Массив с ID треков для главной страницы.
+ * Можно добавлять свои (сколько угодно).
+ */
+let track_list = [
+    "3yPQ0T0w021ucbCqkKBku6",
+    "1RZUlzNWbvCBeoSkapPgVR",
+    "4uyDe8SeM0Nsgzp9KMtXWn",
+    "1zB4vmk8tFRmM9UULNzbLB",
+    "0HqZX76SFLDz2aW8aiqi7G",
+    "4mx0eNjjZUyOzfY3I4P9HD",
+    "3nx1LABlssQw0JCmHTWMn3",
+    "5tz69p7tJuGPeMGwNTxYuV",
+    "0OUZoXhXP1XbJnaez0jSJ4",
+    "7M2tXmeS15NAzEn7ABFeBg"
+];
+
+
+/**
+ * Массив с ID подкастов для главной страницы.
+ * Можно добавлять свои, они появятся в общем списке подкастов на странице.
+ */
+let episodes_list = [
+    "6f9eaEw236X2US8Mf2utY0",
+    "2WiLMdOnfWMaWIKfybMlvl",
+    "0K2myq5sJJzOEzUCtEnpmc",
+    "0ttQ5a6zGcn1d2aoY9HY0H",
+    "2Y2yQPHhxOxRlaLk9ARFGV",
+    "35YSpoUcRx8IuMGHSFzCpk",
+    "2b2f0BDJHoVbhuVHiFt1bt",
+    "5bDg7Ydgc1DXaua4g9gRGB",
+    "7oo9uf8UyDmrB8CJL0KFV7",
+    "0Sr2ouCcw57PcqPNEhVU6p",
+    "4BhvmthskMsH7XOnUtozH9",
+    "6hscGXZHgHwFcV2XIZsUTc",
+    "2ZObWNqdB5EUmuff61syMY",
+    "6UjBd8Z8EsKIBfYQDk7kFQ",
+    "1yAOMjjrINLH0q9ItXaEA2",
+    "6v1H6TV8HL2WrmtC17oXh7"
+];
+
+
+/**
+ * Массив с ID популярных альбомов для главной страницы.
+ * Можно добавлять свои, они появятся в общем списке популярных альбомов на странице.
+ */
+let popular_albums_list = [
+    "3SpBlxme9WbeQdI9kx7KAV",
+    "6s84u2TUpR3wdUv4NgKA2j",
+    "25L8ck3KGcmCo3901ztPzR",
+    "2pqdSWeJVsXAhHFuVLzuA8",
+    "7xV2TzoaVc0ycW7fwBwAml",
+    "6HwzIlrCDq3WF9vMq8meqG",
+    "1nAQbHeOWTfQzbOoFrvndW",
+    "50ez0rCXWIf3s5cvcALStA",
+    "4uVXrwE4aSV2L2aqAHSOXa",
+    "78bpIziExqiI9qztvNFlQu",
+    "4GrFuXwRmEBJec22p58fsD",
+    "4ag07Nbz1xUcFWU17JNh1r",
+    "6tE9Dnp2zInFij4jKssysL",
+    "1kCHru7uhxBUdzkm4gzRQc",
+    "21jF5jlMtzo94wbxmJ18aa",
+    "06HL4z0CvFAxyc27GXpf02",
+    "50Zz8CkIhATKUlQMbHO3k1",
+    "6pOiDiuDQqrmo5DbG0ZubR",
+    "3sKZHtQoq3tPtkXbT8PJAc",
+    "4wOmbyKHZLPlDaLyDWX6wP",
+    "6J84szYCnMfzEcvIcfWMFL",
+    "5YrOK7zze6egKg9a8WRcnD",
+    "4CUvXoN2AD9Al5NnOzaR7W",
+    "5dGWwsZ9iB2Xc3UKR0gif2",
+    "0JGOiO34nwfUdDrD612dOp",
+    "2UJcKiJxNryhL050F5Z1Fk",
+    "4yP0hdKOZPNshxUOjY0cZj",
+    "4SZko61aMnmgvNhfhgTuD3"
+];
+
+export let ids = { 'track_list': track_list, 'episodes_list': episodes_list, 'popular_albums_list': popular_albums_list}
